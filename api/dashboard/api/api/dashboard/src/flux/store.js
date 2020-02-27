0e0c7a9e95d7f6a9e3d6b3bd7bcd20e1a3f6e35f
Alex Montague <xturnermonty@gmail.com>
2020-02-27 11:07:50 -0500

    centralize routes, start to transition to redux

--- api/dashboard/api/api/dashboard/src/flux/store.js
+++ api/dashboard/api/api/dashboard/src/flux/store.js
@@ -2,7 +2,7 @@ import { EventEmitter } from "events";
 
 import Dispatcher from "./dispatcher";
 import Constants from "./constants";
-import getSidebarNavItems from "../data/sidebar-nav-items";
+import getSidebarNavItems from "../routes/sidebar";
 
 let _store = {
   menuVisible: false,
--- api/dashboard/api/api/dashboard/src/routes/sidebar.js
+++ api/dashboard/api/api/dashboard/src/routes/sidebar.js
@@ -1,40 +1,58 @@
 export default function() {
   return [
     {
-      title: "Blog Dashboard",
-      to: "/blog-overview",
+      title: "Holdings",
+      to: "/holdings",
+      htmlBefore: '<i class="material-icons">edit</i>',
+      htmlAfter: ""
+    },
+    {
+      title: "Trade",
+      to: "/trade",
+      htmlBefore: '<i class="material-icons">edit</i>',
+      htmlAfter: ""
+    },
+    {
+      title: "Watchlist",
+      to: "/watchlist",
+      htmlBefore: '<i class="material-icons">edit</i>',
+      htmlAfter: ""
+    },
+    {
+      title: "Alerts",
+      to: "/alerts",
       htmlBefore: '<i class="material-icons">edit</i>',
       htmlAfter: ""
     },
     {
       title: "Blog Posts",
       htmlBefore: '<i class="material-icons">vertical_split</i>',
-      to: "/blog-posts",
+      to: "/blog-posts"
     },
     {
       title: "Add New Post",
       htmlBefore: '<i class="material-icons">note_add</i>',
-      to: "/add-new-post",
+      to: "/add-new-post"
     },
     {
       title: "Forms & Components",
       htmlBefore: '<i class="material-icons">view_module</i>',
-      to: "/components-overview",
+      to: "/components-overview"
     },
     {
       title: "Tables",
       htmlBefore: '<i class="material-icons">table_chart</i>',
-      to: "/tables",
+      to: "/tables"
     },
     {
       title: "User Profile",
       htmlBefore: '<i class="material-icons">person</i>',
-      to: "/user-profile-lite",
+      to: "/user-profile-lite"
     },
     {
       title: "Errors",
       htmlBefore: '<i class="material-icons">error</i>',
-      to: "/errors",
+      to: "/errors"
     }
   ];
 }
