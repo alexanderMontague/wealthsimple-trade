ed9b18873d010daf96c845d87c81987da74e9157
Alex Montague <xturnermonty@gmail.com>
2020-03-26 10:17:14 -0400

    update legal (#8)
    * update legal
    
    * login styles

--- packages/dashboard/src/components/layout/MainFooter.js
+++ packages/dashboard/src/components/layout/MainFooter.js
@@ -4,66 +4,81 @@ import { Container, Row, Nav, NavItem, NavLink } from 'shards-react'
 import { Link } from 'react-router-dom'
 
 const MainFooter = ({ contained, menuItems, copyright }) => (
-    <footer className="main-footer d-flex p-2 px-3 bg-white border-top">
-        <Container fluid={contained}>
-            <Row>
-                <Nav>
-                    {menuItems.map((item, idx) => (
-                        <NavItem key={idx}>
-                            <NavLink tag={Link} to={item.to}>
-                                {item.title}
-                            </NavLink>
-                        </NavItem>
-                    ))}
-                </Nav>
-                <span className="copyright ml-auto my-auto mr-2">
-                    {copyright}
-                </span>
-            </Row>
-        </Container>
-    </footer>
+  <footer className="main-footer d-flex p-2 px-3 bg-white border-top">
+    <Container fluid={contained}>
+      <Row>
+        <Nav>
+          {menuItems.map((item, idx) => (
+            <NavItem key={idx}>
+              <NavLink
+                tag={Link}
+                to={
+                  item.title === 'Wealthsimple'
+                    ? window.location.pathname
+                    : item.to
+                }
+                onClick={
+                  item.title === 'Wealthsimple'
+                    ? () =>
+                        window.open(
+                          'https://www.wealthsimple.com/en-ca/',
+                          '_blank'
+                        )
+                    : () => {}
+                }
+              >
+                {item.title}
+              </NavLink>
+            </NavItem>
+          ))}
+        </Nav>
+        <span className="copyright ml-auto my-auto mr-2">{copyright}</span>
+      </Row>
+    </Container>
+  </footer>
 )
 
 MainFooter.propTypes = {
-    /**
-     * Whether the content is contained, or not.
-     */
-    contained: PropTypes.bool,
-    /**
-     * The menu items array.
-     */
-    menuItems: PropTypes.array,
-    /**
-     * The copyright info.
-     */
-    copyright: PropTypes.string,
+  /**
+   * Whether the content is contained, or not.
+   */
+  contained: PropTypes.bool,
+  /**
+   * The menu items array.
+   */
+  menuItems: PropTypes.array,
+  /**
+   * The copyright info.
+   */
+  copyright: PropTypes.string,
 }
 
 MainFooter.defaultProps = {
-    contained: false,
-    copyright: 'Copyright © 2018 DesignRevision',
-    menuItems: [
-        {
-            title: 'Home',
-            to: '#',
-        },
-        {
-            title: 'Services',
-            to: '#',
-        },
-        {
-            title: 'About',
-            to: '#',
-        },
-        {
-            title: 'Products',
-            to: '#',
-        },
-        {
-            title: 'Blog',
-            to: '#',
-        },
-    ],
+  contained: false,
+  copyright:
+    'All assets, information and accounts owned by Wealthsimple Technologies Inc.',
+  menuItems: [
+    {
+      title: 'Wealthsimple',
+      to: 'https://www.wealthsimple.com/en-ca/',
+    },
+    {
+      title: 'Holdings',
+      to: '/holdings',
+    },
+    {
+      title: 'Trade',
+      to: '/trade',
+    },
+    {
+      title: 'Watchlist',
+      to: '/watchlist',
+    },
+    {
+      title: 'Profile',
+      to: 'user-profile',
+    },
+  ],
 }
 
 export default MainFooter
