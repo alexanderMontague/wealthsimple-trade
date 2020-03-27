b6cd95c194b94ab4570101bcac76008e39fa1ba8
Alex Montague <xturnermonty@gmail.com>
2020-03-27 15:35:43 -0400

    create component

--- packages/dashboard/src/routes/index.js
+++ packages/dashboard/src/routes/index.js
@@ -6,6 +6,7 @@ import { DefaultLayout } from '../layouts'
 
 // Route Views
 import Holdings from '../views/Holdings'
+import Trade from '../views/Trade'
 import BlogOverview from '../views/BlogOverview'
 import UserProfile from '../views/UserProfile'
 import AddNewPost from '../views/AddNewPost'
@@ -16,70 +17,70 @@ import BlogPosts from '../views/BlogPosts'
 import Watchlists from '../views/Watchlists'
 
 export default [
-    {
-        path: '/',
-        exact: true,
-        layout: DefaultLayout,
-        component: () => <Redirect to="/holdings" />,
-    },
-    {
-        path: '/holdings',
-        layout: DefaultLayout,
-        component: Holdings,
-        exact: true,
-    },
-    {
-        path: '/trade',
-        layout: DefaultLayout,
-        component: BlogOverview,
-        exact: true,
-    },
-    {
-        path: '/watchlist',
-        layout: DefaultLayout,
-        component: Watchlists,
-        exact: true,
-    },
-    {
-        path: '/alerts',
-        layout: DefaultLayout,
-        component: BlogOverview,
-        exact: true,
-    },
-    {
-        path: '/user-profile',
-        layout: DefaultLayout,
-        component: UserProfile,
-        exact: true,
-    },
-    {
-        path: '/add-new-post',
-        layout: DefaultLayout,
-        component: AddNewPost,
-        exact: true,
-    },
-    {
-        path: '/errors',
-        layout: DefaultLayout,
-        component: Errors,
-        exact: true,
-    },
-    {
-        path: '/components-overview',
-        layout: DefaultLayout,
-        component: ComponentsOverview,
-        exact: true,
-    },
-    {
-        path: '/tables',
-        layout: DefaultLayout,
-        component: Tables,
-        exact: true,
-    },
-    {
-        path: '/blog-posts',
-        layout: DefaultLayout,
-        component: BlogPosts,
-        exact: true,
-    },
+  {
+    path: '/',
+    exact: true,
+    layout: DefaultLayout,
+    component: () => <Redirect to="/holdings" />,
+  },
+  {
+    path: '/holdings',
+    layout: DefaultLayout,
+    component: Holdings,
+    exact: true,
+  },
+  {
+    path: '/trade',
+    layout: DefaultLayout,
+    component: Trade,
+    exact: true,
+  },
+  {
+    path: '/watchlist',
+    layout: DefaultLayout,
+    component: BlogOverview,
+    exact: true,
+  },
+  {
+    path: '/alerts',
+    layout: DefaultLayout,
+    component: BlogOverview,
+    exact: true,
+  },
+  {
+    path: '/user-profile',
+    layout: DefaultLayout,
+    component: UserProfile,
+    exact: true,
+  },
+  {
+    path: '/add-new-post',
+    layout: DefaultLayout,
+    component: AddNewPost,
+    exact: true,
+  },
+  {
+    path: '/errors',
+    layout: DefaultLayout,
+    component: Errors,
+    exact: true,
+  },
+  {
+    path: '/components-overview',
+    layout: DefaultLayout,
+    component: ComponentsOverview,
+    exact: true,
+  },
+  {
+    path: '/tables',
+    layout: DefaultLayout,
+    component: Tables,
+    exact: true,
+  },
+  {
+    path: '/blog-posts',
+    layout: DefaultLayout,
+    component: BlogPosts,
+    exact: true,
+  },
 ]
